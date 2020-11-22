379345ce4fbcef29497b9fd7d27d5c9a9ec71dfe
Nikolay Zaharov <nzaharov@x570.localdomain>
2020-11-22 22:12:10 +0200

    some working solution

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,108 @@
 #[warn(clippy::all)]
+mod map;
 
-fn main() {
-    println!("Hello, world!");
+use map::*;
+use rand::distributions::WeightedIndex;
+use rand::prelude::*;
+use std::io;
+
+const MAX_GENERATIONS: usize = 30;
+const POPULATION_SIZE: usize = 100;
+
+fn one_point_crossover(p1: &Path, p2: &Path, i: usize) -> Path {
+    let (genes1, genes2) = p1.split_at(i);
+    genes1
+        .iter()
+        .chain(p2.iter().filter(|g| genes2.contains(g)))
+        .cloned()
+        .collect()
+}
+
+fn crossover(p1: &Path, p2: &Path, i: usize) -> Vec<Path> {
+    let child_p1 = one_point_crossover(p1, p2, i);
+    let child_p2 = one_point_crossover(p2, p1, i);
+
+    vec![child_p1, child_p2]
+}
+
+fn mutate(path: &Path, rng: &mut ThreadRng) -> Path {
+    let a = rng.gen_range(0, path.len());
+    let b = rng.gen_range(0, path.len());
+    let mut mutated = path.clone();
+    mutated.swap(a, b);
+    mutated
+}
+
+fn main() -> io::Result<()> {
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer)?;
+    let size = buffer.trim_end().parse::<usize>().expect("Invalid input");
+
+    let mut rng = thread_rng();
+
+    let map = Map::new(size, &mut rng);
+
+    println!("Distances:\n\n{}", map);
+
+    let mut population: Vec<Path> = (0..POPULATION_SIZE)
+        .map(|_| map.get_random_path(&mut rng))
+        .collect();
+    let mut fitness: Vec<f64> = population
+        .iter()
+        .map(|path| 1_f64 / map.get_path_length(path) as f64)
+        .collect();
+
+    for _ in 0..MAX_GENERATIONS {
+        let dist = WeightedIndex::new(&fitness).unwrap();
+        // let index = dist.sample(&mut rng);
+
+        // let alpha_male = population.get(index).unwrap();
+        let index = fitness
+            .iter()
+            .enumerate()
+            .max_by(|(_, a), (_, b)| a.partial_cmp(b).unwrap())
+            .unwrap()
+            .0;
+        let alpha_male = population.get(index).unwrap();
+        let other_indices: Vec<usize> = dist
+            .sample_iter(&mut rng)
+            .take(POPULATION_SIZE / 2)
+            .collect();
+        let crossover_point = rng.gen_range(0, size - 1);
+
+        let children = other_indices
+            .iter()
+            .map(|&i| crossover(alpha_male, population.get(i).unwrap(), crossover_point))
+            .flatten()
+            .map(|child| mutate(&child, &mut rng))
+            .collect::<Vec<Path>>();
+        // let children = population
+        //     .iter()
+        //     .map(|child| mutate(&child, &mut rng))
+        //     .collect();
+
+        population = children;
+        fitness = population
+            .iter()
+            .map(|path| 1_f64 / map.get_path_length(path) as f64)
+            .collect();
+
+        // println!("{:?}", fitness);
+
+        let best_index = fitness
+            .iter()
+            .enumerate()
+            .max_by(|(_, a), (_, b)| a.partial_cmp(b).unwrap())
+            .unwrap()
+            .0;
+        let best_path = population.get(best_index).unwrap();
+
+        println!(
+            "Current best: {:?} {}",
+            best_path,
+            map.get_path_length(best_path)
+        );
+    }
+
+    Ok(())
 }
---
+++ src/map.rs
@@ -0,0 +1,65 @@
+use rand::prelude::*;
+use std::fmt;
+
+const K: usize = 100;
+
+pub type Path = Vec<usize>;
+pub type PathLen = usize;
+
+pub struct Map {
+    city_count: usize,
+    matrix: Vec<Vec<usize>>,
+}
+
+impl Map {
+    pub fn new(size: usize, rng: &mut ThreadRng) -> Self {
+        let mut matrix = vec![vec![0; size]; size];
+
+        for i in 0..matrix.len() {
+            for j in 0..matrix[i].len() {
+                if i != j {
+                    let weight = rng.gen_range(1, K * size);
+                    matrix[i][j] = weight;
+                    matrix[j][i] = weight;
+                }
+            }
+        }
+
+        Self {
+            city_count: size,
+            matrix,
+        }
+    }
+
+    pub fn get_distance(&self, a: usize, b: usize) -> usize {
+        self.matrix[a][b]
+    }
+
+    pub fn get_path_length(&self, path: &Path) -> PathLen {
+        path.iter()
+            .zip(path.iter().skip(1))
+            .fold(self.get_distance(0, path[0]), |acc, (&a, &b)| {
+                acc + self.get_distance(a, b)
+            })
+    }
+
+    pub fn get_random_path(&self, rng: &mut ThreadRng) -> Path {
+        let mut path: Path = (1..self.city_count).collect();
+        path.shuffle(rng);
+        path
+    }
+}
+
+impl fmt::Display for Map {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let result = self.matrix.iter().fold(String::new(), |acc, row| {
+            let row = row
+                .iter()
+                .map(|c| format!("{:>3}", c))
+                .collect::<Vec<String>>()
+                .join(" ");
+            acc + &row + "\n\n"
+        });
+        write!(f, "{}", result)
+    }
+}
